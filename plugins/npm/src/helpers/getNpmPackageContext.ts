b9921e02e68a1938a0d5cdd1fb878d12ff9e2984
Sean Tu <manlao@gmail.com>
2025-03-02 10:25:41 +0800

    fix: resolve resolve-workspace-root (commonjs) import issue

--- plugins/npm/src/helpers/getNpmPackageContext.ts
+++ plugins/npm/src/helpers/getNpmPackageContext.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import-x/default
 import preferredPM from "preferred-pm";
-import { resolveWorkspaceRoot } from "resolve-workspace-root";
+import resolveWorkspaceRootPkg from "resolve-workspace-root";
 
 import { FindPackagesContext, PackageInfo } from "@lets-release/config";
 
@@ -8,6 +8,9 @@ import { getPackage } from "src/helpers/getPackage";
 import { getRegistry } from "src/helpers/getRegistry";
 import { NpmPackageContext } from "src/types/NpmPackageContext";
 import { NpmPackageManager } from "src/types/NpmPackageManager";
+
+// eslint-disable-next-line import-x/no-named-as-default-member
+const { resolveWorkspaceRoot } = resolveWorkspaceRootPkg;
 
 export async function getNpmPackageContext(
   context: Pick<FindPackagesContext, "env"> & {
