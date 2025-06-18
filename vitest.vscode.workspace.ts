fc4861d5c06b5170d92211464bbb399136f52981
Sean Tu <manlao@gmail.com>
2025-06-18 17:36:04 +0800

    chore: remove vitest workspace config

--- vitest.vscode.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from "vitest/config";
-
-export default defineWorkspace([
-  "./vitest.{e2e,unit}.config.ts",
-  "libs/*/vitest.{e2e,unit}.config.ts",
-  "plugins/*/vitest.{e2e,unit}.config.ts",
-]);
